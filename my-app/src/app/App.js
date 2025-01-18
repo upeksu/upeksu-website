759eacb508ad9fe81ca4644b96cf61e56c8e30d4
upeksu <upe.kennesaw@gmail.com>
2025-01-18 00:31:57 -0500

    react and nextjs

---
+++ my-app/src/app/App.js
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+// function App() {
+//   return (
+//     <div className="App">
+//       <CustomNavbar />
+//         <HeroSection />
+//       {/* Add more components here */}
+//     </div>
+//   );
+
+// }
+function App() {
+  return (
+    <div>Hi</div>
+  );
+}
+
+export default App;
